b7f5634e7aea1324ede2940553b125667628c323
weghz <weghzan@gmail.com>
2022-11-20 12:26:59 +0100

    new server

--- server2.py
+++ server2.py
@@ -1,23 +1,37 @@
-import socket               # Import socket module
+import socket  # Import du module socket
+import connexionBD as BD
 
-soc = socket.socket()         # Create a socket object
-host = "localhost" # Get local machine name
-port = 2005                # Reserve a port for your service.
-soc.bind((host, port))       # Bind to the port
-soc.listen(5)                 # Now wait for client connection.
+
+soc = socket.socket()  # Creation d'un objet socket
+host = "localhost"  # Nom de l'hote
+port = 2004  # on reserve un port
+soc.bind((host, port))  # Liaison avec le port
+soc.listen(4)  # En attente d'une connexion avec nombre de client maximum
+print ("serveur lancé")
 while True:
-	conn, addr = soc.accept()     # Establish connection with client.
-	print ("Got connection from",addr)
-	msg = conn.recv(1024)
-	msg = msg.decode('utf-8')
-	msg = msg.strip()
-	print (msg)
-	print("longueur : "+str(len(msg)))
-	if ( msg == 'Hello'):
-		print("Hii everyone")
-		conn.send("Hello Client!\n".encode())
-		conn.send("Lol\n".encode())
-		
-	else:
-		print("Go away")
-	conn.close()
+    conn, addr = soc.accept()  # Etablissement de la connexion
+    print("connexion etablie :", addr)
+    msg = conn.recv(1024)
+    msg = msg.decode("utf-8")
+    msg = msg.strip()
+    if msg == 'HELLO':
+        print("client: " + msg)
+        cur = BD.connBD.cursor()
+        conn.send("id : ?\n".encode())
+        msg = conn.recv(1024)
+        msg = msg.decode("utf-8")
+        msg = msg.strip()
+        sql_query = 'SELECT * FROM utilisateur WHERE id_user=%s'
+        try:
+            cur.execute(sql_query, (msg,))
+        except:
+            print("impossible d'executer la requete")
+        if cur.rowcount==1:
+            conn.send("je te connais\n".encode())
+            msg = conn.recv(1024)
+            msg = msg.decode("utf-8")
+            msg = msg.strip()
+        else :
+            conn.send('je te connais pas\n'.encode())
+    conn.close()
+
