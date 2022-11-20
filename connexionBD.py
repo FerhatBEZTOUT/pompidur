4e305d536382cf288cbea3eebc2ed3628c10d68b
weghz <weghzan@gmail.com>
2022-11-20 12:18:20 +0100

    bddconn

---
+++ connexionBD.py
@@ -0,0 +1,9 @@
+import psycopg2
+try:
+    connBD = psycopg2.connect("dbname='pampidur_bdd' user='pampidur' host='postgresql-pampidur.alwaysdata.net' password='Ma3dnous!!'")
+    print('connexion réussie')
+except Exception as error:
+    print(error)
+    print('\nImpossible de se connecter au serveur BD')
+    exit()
+
